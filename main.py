70fac323209b88a36fd3dd95c31470b32c085819
kuwjr <kuwjr@outlook.com>
2022-02-03 10:07:53 +0530

    load urls from array

--- main.py
+++ main.py
@@ -3,27 +3,32 @@ import csv
 from utils import checkCms
 from woocommerce import get_links, get_productdata
 
-# load url 
-url = 'https://theparfumerie.lk/shop'
+urls = ['lk.spaceylon.com', 'zigzag.lk', 'zigmajones.com', 'theparfumerie.lk', 'kikibeauty.lk']
 
-# set headers to recreate browser view
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
-}
+for u in urls:
+    # load url 
+    url = "https://{0}/shop".format(u)
 
-# detect cms
-cms = checkCms(url, headers)
+    # set headers to recreate browser view
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+    }
 
-if cms == False:
-    print("There was an error! Meow!")
-else:
-    # based on cms, run extraction
-    if cms['name'] == 'woocommerce':
-        # send web page
-        links = get_links(url)
+    # detect cms
+    cms = checkCms(url, headers)
 
-        results = []
-        for link in links:
-            results.append(get_productdata(link))
-            print("\n")
-            time.sleep(1)
+    print("\n\nMeow:{0}\n\n".format(url))
+
+    if cms == False:
+        print("\n\nError:{0}\n\n".format(url))
+    else:
+        # based on cms, run extraction
+        if cms['name'] == 'woocommerce':
+            # send web page
+            links = get_links(url)
+
+            results = []
+            for link in links:
+                results.append(get_productdata(link))
+                print("\n")
+                time.sleep(1)
